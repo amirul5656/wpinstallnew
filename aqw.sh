f83b2317f1443634ee9c7b3e148fbea05c7404ab
amirul5656 <hendra.amirul@gmail.com>
2025-05-12 08:33:18 +0700

    Add files via upload

---
+++ aqw.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./aqw --algorithm xelishashv2_pepew;mike;argon2d_16000;yespowertide --pool stratum+tcp://xelisv2-pepew.sea.mine.zpool.ca:4833;stratum+tcp://mike.sea.mine.zpool.ca:5356;stratum+tcp://argon2d16000.sea.mine.zpool.ca:4241;stratum+tcp://yespowerTIDE.sea.mine.zpool.ca:6239 --wallet D8n4HXcTza73pBw93ZnSgcZRo8rHdZJLux;D8n4HXcTza73pBw93ZnSgcZRo8rHdZJLux;D8n4HXcTza73pBw93ZnSgcZRo8rHdZJLux;D8n4HXcTza73pBw93ZnSgcZRo8rHdZJLux --password c=DOGE;c=DOGE;c=DOGE;c=DOGE --cpu-threads 6
